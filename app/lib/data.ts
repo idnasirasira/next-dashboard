ec1b342ca2884187c6c1622e46bb98155737ea31
Aris Arisandi <idnasirasira@gmail.com>
2024-09-19 11:48:35 +0700

    Chapter 9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -29,6 +29,9 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  console.log('Fetching revenue data...');
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+  console.log('Data fetch completed after 3 seconds.');
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
